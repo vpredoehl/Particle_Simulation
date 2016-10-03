c7f6013c1a42f39c6ab2b8685efd7f52d3f83799
Vincent J Predoehl <vpredoehl@gmail.com>
2016-10-03 16:45:50 -0500

    add wrapper class for communication data types

--- tutorials/mpi-send-and-receive/code/MPIntf.h
+++ tutorials/mpi-send-and-receive/code/MPIntf.h
@@ -12,6 +12,21 @@ namespace CSE856
 		int rank() const	{	return world_rank;	}
 		int size() const	{	return world_size;	}
 
+            // wrapper class for communication types
+	template<class T>
+            class CommType
+            {
+                T v;
+            public:
+            
+                T value() const {   return v;   }
+                operator T() const  {   return v;   }
+                const T* operator&() const    {   return &v;  }
+                T* operator&()    {   return &v;  }
+                
+                CommType& operator=(T v)    {   CommType::v = v;  return *this;   }
+            };
 	};
+
 
 }
